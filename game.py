5e413f038925a36e6e82d8e74113c55652231d97
safnas saleem <safnas@hamon.in>
2022-02-10 00:33:40 +0530

    added passed cases

--- game.py
+++ game.py
@@ -1,25 +1,115 @@
-maximumlength = 3
-def grid_view(list):
+from collections import Counter
+maximumlength = 4
+def grid_view(l):
     col_count = 0
     list2 =[]
-    for i in range(0, maximumlength):
+    for i in range(0, 4):
         list1 = []
-        for j in range(0, maximumlength):
-            if(col_count in list):
+        for j in range(0, 4):
+            if(col_count in l):
                 list1.append(True)
             else:
                 list1.append(False)
             col_count += 1
         list2.append(list1)
-
     return list2
 
-def position_true(list):
+def position_true(l):
     pos = []
-    for i in range(0, maximumlength):
-        for j in range(0, maximumlength):
-            p = []
-            if(list[i][j] == True):
-                p.extend([i,j])
-                pos.append(p)
+    for i in range(0, 4):
+        for j in range(0, 4):
+            position= []
+            if(l[i][j] == True):
+                position.extend([i,j])
+                pos.append(position)
     return pos
+
+def neighbours(list,i):
+    true = []
+    false = []
+    if (i[1]-1 >= 0 and i[0]-1 >= 0):
+        if( list[i[0]-1][i[1]-1] == True):
+            true.append('y')
+        elif (list[i[0]-1][i[1]-1] == False):
+            pos=[]
+            pos.extend([i[0]-1,i[1]-1])
+            false.append(pos)
+        
+    if (i[0]-1 >= 0):
+        if(list[i[0]-1][i[1]] == True):
+            true.append('y')
+        elif (list[i[0]-1][i[1]] == False):
+            pos=[]
+            pos.extend([i[0]-1, i[1]])
+            false.append(pos)
+                
+    if (i[1]+1 < 4 and i[0]-1 >= 0):
+        if( list[i[0]-1][i[1]+1] == True):
+            true.append('y')
+        elif (list[i[0]-1][i[1]+1] == False):
+            pos=[]
+            pos.extend([i[0]-1, i[1]+1])
+            false.append(pos)
+
+    if (i[1]-1 >= 0):
+        if( list[i[0]][i[1]-1] == True):
+            true.append('y')
+        elif (list[i[0]][i[1]-1] == False):
+            pos=[]
+            pos.extend([i[0], i[1]-1])
+            false.append(pos)
+        
+    if (i[1]+1 < 4):
+        if(list[i[0]][i[1]+1] == True):
+            true.append('y')
+        elif (list[i[0]][i[1]+1] == False):
+            p=[]
+            p.extend([i[0], i[1]+1])
+            false.append(p)
+        
+    if (i[1]-1 >= 0 and i[0]+1 < 4):
+        if(list[i[0]+1][i[1]-1] == True):
+            true.append('y')
+        elif (list[i[0]+1][i[1]-1] == False):
+            p=[]
+            p.extend([i[0]+1, i[1]-1])
+            false.append(p)
+
+    if (i[0]+1 < 4):
+        if(list[i[0]+1][i[1]] == True):
+            true.append('y')
+        elif (list[i[0]+1][i[1]] == False):
+            p=[]
+            p.extend([i[0]+1, i[1]])
+            false.append(p)
+               
+    if (i[1]+1 < 4 and i[0]+1 < 4):
+        if(list[i[0]+1][i[1]+1] == True):
+            true.append('y')
+        elif (list[i[0]+1][i[1]+1] == False):
+            p=[]
+            p.extend([i[0]+1, i[1]+1])
+            false.append(p)
+    
+    return len(true), false
+
+
+def change_status(list):
+    new_list = list.copy()
+    x = position_true(list)
+    false_nb = []
+    die = []
+    for i in x:
+        true_cells, false_cells = neighbours(list,i)
+        if(true_cells < 2 or true_cells > 3):
+            die.append(i)
+        false_nb.extend(false_cells)
+    for i in die:
+        list[i[0]][i[1]] = False
+    count = Counter([tuple(i) for i in false_nb])
+    for key, value in count.items():
+        if (value == 3):
+           new_list[key[0]][key[1]]=True
+
+    return new_list
+
--- game_test.py
+++ game_test.py
@@ -1,25 +1,64 @@
 import game
 
-def test_game_grid_no_input():
-    expected_value = [[False, False, False],[ False, False,False],[ False,False,False]]
+def testgame_grid_no_input():
+    expected_value = [[False, False, False,False],[ False, False,False, False],[ False,False, False, False],[False, False, False,False]]
     assert game.grid_view([]) == expected_value
     
-def test_game_grid_input():
-    expected_value = [[False, True, True],[ False, True,False],[False, False, False]]
-    assert game.grid_view([1,2,4]) == expected_value
+def testgame_grid_input():
+    expected_value = [[False, True, False,True],[ False, True,False, False],[ False,False, False, False],[False, False, False,False]]
+    assert game.grid_view([1,3,5]) == expected_value
 
-def test_game_grid_repeat_input():
-    expected_value = [[False, True, True],[ False, True,False],[ False,False, False]]
-    assert game.grid_view([1,2,4]) == expected_value
+def testgame_grid_repeat_input():
+    expected_value = [[False, True, False,True],[ False, True,False, False],[ False,False, False, False],[False, False, False,False]]
+    assert game.grid_view([1,3,5,1]) == expected_value
 
-def test_position_of_true_no_value(): 
-    assert game.position_true( [[ False, False,False],
-                                [ False,False, False],
-                                [ False, False, False],
-                                ]) == []
 
-def test_position_of_true_multiple(): 
-    assert game.position_true( [[ True, False,True],
-                                [ False,False, False],
-                                [ False, True, False]
-                                ]) == [[0,0],[0,2],[2,1]]
+def testposition_of_true_no_value(): 
+    assert game.position_true( [[ False, False,False, False],
+                                [ False,False, False, False],
+                                [ False, False, False, False],
+                                [False, False, False,False]]) == []
+
+def testposition_of_true_multiple(): 
+    assert game.position_true( [[ True, False,True, False],
+                                [ False,False, False, False],
+                                [ False, True, False, False],
+                                [False, False, False,False]]) == [[0,0],[0,2],[2,1]]
+
+
+def test_no_neighbour():
+    def test_no_neighbour():
+
+        nb_num, false_nb = game.neighbours( [[ True, False,True, False],
+                                         [ False,False, False, False],
+                                         [ False, True, False, False],
+                                         [False, False, False,False]], [0,0])
+        assert nb_num == 0
+        assert  false_nb == [[0,1],[1,0],[1,1]]
+
+def test_3_neighbours():
+   nb_num, false_nb = game.neighbours( [[ True, False,True, False],
+                                        [ False,True, False, False],
+                                        [ False, True, False, False],
+                                        [False, False, False,False]], [1,1])
+   assert nb_num == 3
+   assert  false_nb == [[0,1],[1,0],[1,2],[2,0],[2,2]]
+
+def test_no_status_change():
+   assert game.change_status( [[ True, True, False, False],
+                               [ True,True, False, False],
+                               [ False, False, False, False],
+                               [False, False, False,False]]) == [[ True, True, False, False],
+                               [ True,True, False, False],
+                               [ False, False, False, False],
+                               [False, False, False,False]]
+   
+def test_status_change():
+    assert game.change_status( [[ False, False, False, False],
+                               [ True,True, True, False],
+                               [ False, False, False, False],
+                               [False, False, False,False]]) == [[  False, True, False, False],
+                               [ False,True, False, False],
+                               [ False, True, False, False],
+                               [False, False, False,False]]
+
